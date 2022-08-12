08d48e29a15a7d3f304b070f42fe10b7b0541507
zefinity <codespaceproduction@gmail.com>
2022-08-12 15:36:39 +0700

    feat: add sql migration

---
+++ migrations/001_initial_migration.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS public."user"
+(
+    id bigint NOT NULL DEFAULT nextval('user_id_seq'::regclass),
+    username text COLLATE pg_catalog."default" NOT NULL,
+    password text COLLATE pg_catalog."default" NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    CONSTRAINT user_pkey PRIMARY KEY (id),
+    CONSTRAINT unique_username UNIQUE (username)
+);
+
+CREATE TABLE IF NOT EXISTS public.post
+(
+    id bigint NOT NULL DEFAULT nextval('post_id_seq'::regclass),
+    title text COLLATE pg_catalog."default",
+    content text COLLATE pg_catalog."default",
+    is_public boolean NOT NULL DEFAULT false,
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    created_by bigint NOT NULL,
+    updated_at timestamp with time zone NOT NULL DEFAULT now(),
+    updated_by bigint,
+    is_deleted boolean NOT NULL DEFAULT false,
+    CONSTRAINT post_pkey PRIMARY KEY (id),
+    CONSTRAINT post_created_by_fkey FOREIGN KEY (created_by)
+        REFERENCES public."user" (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID
+);
